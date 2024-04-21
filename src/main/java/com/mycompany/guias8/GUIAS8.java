01ed8e0aa52cf823cc16e66fc46aa93a991af7a7
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:57:48 -0500

    primer GUIA8

---
+++ src/main/java/com/mycompany/guias8/GUIAS8.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias8;
+
+import igui.pantalla8;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS8 {
+
+    public static void main(String[] args) {
+         new pantalla8().setVisible(true);
+    }
+}
